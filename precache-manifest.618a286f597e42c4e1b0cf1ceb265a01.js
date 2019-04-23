e4c94c99b23b3a67841c2929cf1485841abfde8c
Brent Groves <brent.groves@gmail.com>
2019-04-23 14:50:21 -0400

    Updates

---
+++ precache-manifest.618a286f597e42c4e1b0cf1ceb265a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac2177895b82bace4d9b40c386ff8f5",
+    "url": "/mern-todo-app/index.html"
+  },
+  {
+    "revision": "e8e434e7fddfa2e91084",
+    "url": "/mern-todo-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mern-todo-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e8e434e7fddfa2e91084",
+    "url": "/mern-todo-app/static/js/main.a0249ae2.chunk.js"
+  },
+  {
+    "revision": "a4c2b9118c48a20eb484",
+    "url": "/mern-todo-app/static/js/runtime~main.63dec340.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
