7600c61cb4b9cdd323767accefd439e99b89530b
Brent Groves <brent.groves@gmail.com>
2019-04-24 15:08:02 -0400

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
-import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Navigation} from './main/containers/Navigation';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+import CreateTodo from "./main/components/create-todo.component";
+import EditTodo from "./main/components/edit-todo.component";
+import TodosList from "./main/components/todos-list.component";
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <main className="main-content col-lg-12 col-md-12 col-sm-12 p-0">
+			<Router>
+                        <Navigation/>
+                        <div class="main-content-container container-fluid px-4">
+                            <Route  path="/plantInfo" component={CreateTodo}  />
+                        </div>
+			</Router>
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
+export default App;
