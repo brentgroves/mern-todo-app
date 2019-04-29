4a3176321cad656ded8e3b6ff6e08323cd7f7652
brent groves <brent.groves@gmail.com>
2019-04-29 19:58:36 -0400

    modified frontend

--- src/main/components/create-todo.component.js
+++ src/main/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
 	constructor(props) {
@@ -41,7 +42,24 @@ export default class CreateTodo extends Component {
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
-        
+       
+		const newTodo = {
+			todo_description: this.state.todo_description,
+			todo_responsible: this.state.todo_responsible,
+			todo_priority: this.state.todo_priority,
+			todo_completed: this.state.todo_completed
+		};
+
+		axios.post('http://localhost:4000/todos/add', newTodo).then(res => console.log(res));
+/*
+		axios.post('http://localhost:4000/todos/add', newTodo)
+		  .then(function (response) {
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		    console.log(error);
+		  });
+*/		
         this.setState({
             todo_description: '',
             todo_responsible: '',
